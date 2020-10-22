45b7fb6522841d5d02d3dadcf52595a8e37819d2
arladnice <darlakov@mail.ru>
2020-10-22 19:57:51 +0300

    layout

--- ContactsApp/app/src/main/java/com/example/contactsapp/MainActivity.kt
+++ ContactsApp/app/src/main/java/com/example/contactsapp/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.contactsapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    class ContactAdapter(items: List<String>, ctx: Context) :
+        RecyclerView.Adapter<ContactAdapter.ViewHolder>() {
+
+        private var list = items
+        private var context = ctx
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ContactAdapter.ViewHolder, position: Int) {
+
+        }
+
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): ContactAdapter.ViewHolder {
+            return ViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.contact_child, parent, false)
+            )
+        }
+        class ViewHolder(v: View) : RecyclerView.ViewHolder(v){
+
+        }
     }
 }
