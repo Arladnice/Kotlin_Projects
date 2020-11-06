a625e4fb9d7248b8a97b82086947887402189a00
arladnice <darlakov@mail.ru>
2020-11-07 01:39:29 +0300

    update

--- NavigationUpdated/app/src/main/java/com/example/navigationupdated/HomeFragment.kt
+++ NavigationUpdated/app/src/main/java/com/example/navigationupdated/HomeFragment.kt
@@ -1,18 +1,17 @@
 package com.example.navigationupdated
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
